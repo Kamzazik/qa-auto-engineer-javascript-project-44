0a0bc6cc068c826ec718ef4e1c1e07be6e0e4df4
Никита <nikitanemcov34@gmail.com>
2026-02-09 11:09:59 +0300

    feat: add brain-gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+// Функция для нахождения НОД (алгоритм Евклида)
+const findGcd = (a, b) => {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
+
+// Генерация случайного числа
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Основная логика игры
+const runGcdGame = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(findGcd(num1, num2));
+  
+  return { question, correctAnswer };
+};
+
+export default runGcdGame;
