4fd039fe7cec407942fdb6e5ffb445b9459ae63b
Никита <nikitanemcov34@gmail.com>
2026-02-09 10:59:23 +0300

    fix: correct import in brain-calc binary

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-import runCalcGame from '../src/games/calc.js';
+import runBrainCalcGame from '../src/games/brain-calc.js';
 
 const gameDescription = 'What is the result of the expression?';
 
-runGame(gameDescription, runCalcGame);
+runGame(gameDescription, runBrainCalcGame);
